9459c26f3d17e423ce86ac8da6c0634a377c7a0b
selly361 <seikou615@gmail.com>
2025-12-10 19:11:59 +0000

    feat: add DaySelector component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,2 @@
 export * from './UnitsMenu'
+export * from './DaySelector'
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,5 +1,9 @@
+import { DAYS } from "../constants";
+
 export interface UnitSettings {
   temp: "c" | "f";
   wind: "kmh" | "mph";
   precip: "mm" | "in";
 }
+
+export type DayName = (typeof DAYS)[number]
